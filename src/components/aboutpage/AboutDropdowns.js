e85e1c2b6d3480271dc23b7f482a915cafee0eab
Romain-6793 <96068766+Romain-6793@users.noreply.github.com>
2022-10-11 13:38:08 +0200

    before API setting

---
+++ src/components/aboutpage/AboutDropdowns.js
@@ -0,0 +1,16 @@
+import '../../styles/locationpage/LocationDropdowns.css'
+import Dropdown2 from './Dropdown2'
+
+
+function LocationDropdowns() {
+    return (
+        <div className="dropdowns-wrapper2">
+            <Dropdown2 />
+            <Dropdown2 />
+            <Dropdown2 />
+            <Dropdown2 />
+        </div>
+    )
+}
+
+export default LocationDropdowns
--- src/components/aboutpage/AboutMainWrapper.js
+++ src/components/aboutpage/AboutMainWrapper.js
@@ -1,12 +1,14 @@
 import "../../styles/MainWrapper.css"
 import AboutBanner from './AboutBanner'
+import AboutDropdowns from './AboutDropdowns'
+
 
 
 
 function AboutMainWrapper() {
     return (
         <div className="main-wrapper">
-            <AboutBanner />
+            <AboutBanner /><AboutDropdowns />
         </div>
     )
 }
