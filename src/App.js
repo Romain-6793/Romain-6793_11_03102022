0ff289c5a0ecf60ef3b8b57ce3dbe1fc23fb43db
Romain-6793 <96068766+Romain-6793@users.noreply.github.com>
2022-10-12 12:02:57 +0200

    before refactoring body style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import LocationPage from "./pages/LocationPage";
 import NoPage from "./pages/NoPage";
+import kasa from './datas/datas.json';
 
 
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setData(kasa)
+  }, [])
+  console.log(data)
+
+
   return (
     <BrowserRouter>
       <Routes>
